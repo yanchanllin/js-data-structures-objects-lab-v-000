ff921ccddbeeb49d60d805baa63a177c833dc18d
YanC Lin <yonalin1@gmail.com>
2018-08-21 11:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({},driver,{[key]: value});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver,{[key]: value});
+}
