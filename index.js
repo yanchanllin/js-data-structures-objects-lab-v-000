840b778ebca3d3cac8f3427645ea127decca1df2
YanC Lin <yonalin1@gmail.com>
2018-08-21 11:33:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your solution in this file!
-const obj = driver;
-function updateDriverWithKeyAndValue() {
-  obj = { name: 'Sam',
-  address: '11 Broadway' };
+const driver = {};
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign({},driver,{[key]: value});
 }
